98555f8690d0663754c958423494b805848d75c2
Susan Liu <susanliu@Susans-MacBook-Pro-2.local>
2021-08-06 15:14:28 -0400

    cleaned up cpp and added elements to .gitignore

--- src/c++/RetinaNvsModel/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/susanliu/Documents/AndreouResearch/RetinaNVSmodel/src/c++/RetinaNvsModel
-# Build directory: /Users/susanliu/Documents/AndreouResearch/RetinaNVSmodel/src/c++/RetinaNvsModel
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
