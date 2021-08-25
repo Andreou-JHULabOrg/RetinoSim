2384e54b14375f8590d47c0c7f43d305e2e1876f
js3ng <jonah.sengupta@gmail.com>
2021-08-25 16:24:35 -0400

    added functionality to generate entire dataset from JHUMMA

--- src/matlab/test/test_Gait2Events_genscript0.m
+++ src/matlab/test/test_Gait2Events_genscript0.m
@@ -8,38 +8,38 @@ addpath(genpath('../aux'));
 addpath(genpath('../io'));
 
 %%
-% 
-% trials = {
-%     'bb_1', ...
-%     'dm_1', ...
-% %     'dm_2', ...
-%     'gg_1', ...
-% %     'gg_2', ...
-%     'gt_1', ...
-%     'jm_1', ...
-%     'jz_1', ...
-%     'ks_1', ...
-%     'ks_2', ...
-%     'mc_1', ...
-% %     'tf_1', ...
-%     'tm_1', ...
-%     'tm_2'
-%     };
-
 
 trials = {
     'bb_1', ...
     'dm_1', ...
+    'dm_2', ...
     'gg_1', ...
+    'gg_2', ...
     'gt_1', ...
     'jm_1', ...
     'jz_1', ...
     'ks_1', ...
     'ks_2', ...
     'mc_1', ...
+    'tf_1', ...
     'tm_1', ...
     'tm_2'
     };
+
+
+% trials = {
+%     'bb_1', ...
+%     'dm_1', ...
+%     'gg_1', ...
+%     'gt_1', ...
+%     'jm_1', ...
+%     'jz_1', ...
+%     'ks_1', ...
+%     'ks_2', ...
+%     'mc_1', ...
+%     'tm_1', ...
+%     'tm_2'
+%     };
 
 mvmts = {
 '_walk_facing_forward_N_S', ...
@@ -91,7 +91,7 @@ params.inject_poiss_noise       = 0;
 params.write_frame = 0;
 params.write_frame_tag = 'leakrate_5_diffnet_1';
 
-run = 'run_02';
+run = 'run_03';
 outputDirectory = ['/home/jonahs/projects/ReImagine/AER_Data/model_output/gait/' run];
 
 save([outputDirectory '/mats/genscript0_' run '_params.mat'],'params');
@@ -107,49 +107,50 @@ for curTidx = 1:length(trials)
 		
 		filenamePrefix = [trials{curTidx} mvmts{curMidx}];
 		videoFile = ['/DatasetsStaging/ONR-MURI-2009/JHUMMA-Shriver_Aug2014/make_rgb_videos/videos/' filenamePrefix '.avi'];
-		inVid = readVideo_rs( videoFile, params.nrows, params.ncols, params.numFrames, 1 );
-		
-		fprintf('[genscript-INFO] Generating events from video file: %s.avi\n', filenamePrefix);
-		
-		% ------------------------------------------------ 2. PROCESS VIDEO 
-		
-		[TD, eventFrames, ~, grayFrames, curFrames] = RetinaNvsModel(double(inVid), params);
-		
-		% ------------------------------------------------- 3. SAVE OUTPUTS
-		
-		outframes = videoBlend(inVid, eventFrames, 0, 0, 'test.avi');
-		
-		save([outputDirectory '/mats/' filenamePrefix '_events_' run '.mat'],'TD');
-		
-		v = VideoWriter([outputDirectory '/vids/' filenamePrefix '_event_frames_' run '.avi']);
-		open(v);
-		for k = 1:size(eventFrames,4)
-			imagesc(eventFrames(:,:,:,k));
-% 			pause(1/10);
-			M = getframe(gcf);
-			writeVideo(v,M);
+		try
+			inVid = readVideo_rs( videoFile, params.nrows, params.ncols, params.numFrames, 1 );
+			
+			fprintf('[genscript-INFO] Generating events from video file: %s.avi\n', filenamePrefix);
+			
+			% ------------------------------------------------ 2. PROCESS VIDEO
+			
+			[TD, eventFrames, ~, grayFrames, curFrames] = RetinaNvsModel(double(inVid), params);
+			
+			% ------------------------------------------------- 3. SAVE OUTPUTS
+			
+			outframes = videoBlend(inVid, eventFrames, 0, 0, 'test.avi');
+			
+			save([outputDirectory '/mats/' filenamePrefix '_events_' run '.mat'],'TD');
+			
+			v = VideoWriter([outputDirectory '/vids/' filenamePrefix '_event_frames_' run '.avi']);
+			open(v);
+			for k = 1:size(eventFrames,4)
+				currentRGBFrame = (eventFrames(:,:,:,k));
+				scaledFrame = uint8(rescale(currentRGBFrame,0,255));
+				writeVideo(v,scaledFrame);
+			end
+			close(v);
+			
+			v = VideoWriter([outputDirectory '/vids/' filenamePrefix '_gray_frames_' run '.avi']);
+			open(v);
+			for k = 1:size(inVid,3)
+				currentRGBFrame = (inVid(:,:,k));
+				scaledFrame = uint8(currentRGBFrame);
+				writeVideo(v,scaledFrame);
+			end
+			close(v);
+			
+			v = VideoWriter([outputDirectory '/vids/' filenamePrefix '_blended_frames_' run '.avi']);
+			open(v);
+			for k = 1:size(outframes,4)
+				currentRGBFrame = outframes(:,:,:,k);
+				scaledFrame = uint8(currentRGBFrame);
+				writeVideo(v,scaledFrame);
+			end
+			close(v);
+		catch ME
+			warning('Video file not found');
 		end
-		close(v);
-		
-		v = VideoWriter([outputDirectory '/vids/' filenamePrefix '_gray_frames_' run '.avi']);
-		open(v);
-		for k = 1:size(inVid,3)
-			imagesc(inVid(:,:,k));
-% 			pause(1/10);
-			M = getframe(gcf);
-			writeVideo(v,M);
-		end
-		close(v);
-		
-		v = VideoWriter([outputDirectory '/vids/' filenamePrefix '_blended_frames_' run '.avi']);
-		open(v);
-		for k = 1:size(outframes,4)
-			imagesc(outframes(:,:,:,k));
-% 			pause(1/10);
-			M = getframe(gcf);
-			writeVideo(v,M);
-		end
-		close(v);
 
 		
 	end
