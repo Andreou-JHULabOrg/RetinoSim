49cfb11eb4add8af16490c57a05b3cb66bc66015
Jonah Sengupta <jonah.sengupta@gmail.com>
2021-12-31 11:30:18 -0500

    added comments, current waveforms, and nominal parameters for SRA

--- src/matlab/modeling/SRAneuron.m
+++ src/matlab/modeling/SRAneuron.m
@@ -1,21 +1,36 @@
 clear;clc;
 
 vmem    = 10;
-imem    = [2*ones(1,100) 5*ones(1,100) 7*ones(1,100) 10*ones(1,100)];
+% imem    = [2*ones(1,100) 5*ones(1,100) 7*ones(1,100) 10*ones(1,100)];
+% discrete time membrane input current
+imem    = [10*ones(1,100) 0*ones(1,200) 7*ones(1,300) 6*ones(1,200) 0*ones(1,200)];
+
+% reset membrane voltage upon spike
 vrst    = -70;
+% threshold membrane voltage that generates spikes
 vth     = 15;
 
-tleak = 10;
+% membrane (leakage) time constant
+tleak = 8;
+
+% membrane leak potential (steady state)
+Eleak = -22;
 Esra = 0;
 
-g0 = 0.05;
+% spike rate adaptation dynamic parameters
+
+%incremental conductance addition (upon spike) :increase value to arrive at
+%equilibrium condition with quicker
+g0 = 0.06; 
+%initial conductance state
 gsra = 0;
-tsra = 2;
+%SRA time constant, increase value to lengthen decay time
+tsra = 128;
 
 gsra_states(1) = gsra;
 vmem_states(1) = vmem;
 
-tend = 400;
+tend = 1000;
 
 t0 = 1;
 
@@ -24,14 +39,22 @@ for t=2:tend
     
     dgsra = -gsra/tsra;
     
-    dvmem = imem_cur- vmem/tleak + gsra*(Esra-vmem);
-    
+    % Uncomment to use LIF model 
+%     dvmem = Eleak/tleak - vmem/tleak + imem_cur;
+
+    % Uncomment to use SRA model 
+    dvmem = Eleak/tleak - vmem/tleak + imem_cur + gsra*(Esra-vmem);
+
+
+    % Euler method DE update
     vmem = vmem + dvmem;
     gsra = gsra + dgsra;
     
+    % Store system states
     gsra_states(t) = gsra;
     vmem_states(t) = vmem;
-
+    
+    % Non-linear reset update
     if vmem>vth
         gsra = gsra + g0;
         vmem = vrst;
@@ -44,5 +67,7 @@ end
 figure();
 subplot(2,1,1)
 plot(1:tend, vmem_states);
+title('Vmem');
 subplot(2,1,2)
 plot(1:tend, gsra_states);
+title('Gsra');
